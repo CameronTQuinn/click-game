53570bf27ddce075ed753d849d5b1097385c9ae2
Leitet <johan.leitet@lnu.se>
2017-07-04 09:29:06 +0200

    added css for authenticity

---
+++ client/source/js/app.js
@@ -0,0 +1,5 @@
+/*  You can safley remove the next line. This is just to show that using webpack you
+    can load assets using require or import
+    Read more: https://webpack.js.org/guides/asset-management/#loading-css
+    */
+require('../css/style.css')
