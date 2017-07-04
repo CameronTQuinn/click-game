7ff0de572e854c679bfa93bf29acb4194cce6036
Leitet <johan.leitet@lnu.se>
2017-07-04 09:42:28 +0200

    Solution

--- client/source/js/app.js
+++ client/source/js/app.js
@@ -1,5 +1,9 @@
-/*  You can safley remove the next line. This is just to show that using webpack you
-    can load assets using require or import
-    Read more: https://webpack.js.org/guides/asset-management/#loading-css
-    */
-require('../css/style.css')
+
+var lnuit = require('./lnuit')
+
+// Add the style to the head
+var relativePath = './css/style.css'
+
+// call the two functions needed for this exercise
+lnuit.addStyleFile(relativePath)
+lnuit.addClassToAllLinks('lnu')
