ca84d709917cd84cef0a2eaf671ef282a02335bc
Leitet <johan.leitet@lnu.se>
2017-07-04 09:47:19 +0200

    opps

--- client/source/js/app.js
+++
@@ -1,8 +0,0 @@
-var lnuit = require('./lnuit')
-
-// Add the style to the head
-var relativePath = './css/style.css'
-
-// call the two functions needed for this exercise
-lnuit.addStyleFile(relativePath)
-lnuit.addClassToAllLinks('lnu')
